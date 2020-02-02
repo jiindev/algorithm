38c2ab4415ee511d89ced12e8a6848cea99611e6
jiindev <jiindev@gmail.com>
2020-02-02 13:44:17 +0900

    200202 매개변수/결과값 추가

--- jiin/200202_평균구하기.js
+++ jiin/200202_평균구하기.js
@@ -1,8 +1,8 @@
-function solution(arr) {
+function solution(arr) { //arr = [2,5,1,5,7]
     let total = 0;
     for(let i=0; i<arr.length; i++){
         total  = total+arr[i];
     }
     let result = total / arr.length;
-    return result;
+    return result; //4
 }
