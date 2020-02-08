3875d67de2e50bb3a332424a17001ce5bcb0ea28
jiindev <jiindev@gmail.com>
2020-02-08 20:02:30 +0900

    jiin / 서울에서 김서방 찾기

---
+++ jiin/200208_서울에서김서방찾기.js
@@ -0,0 +1,7 @@
+const solution = (seoul) => { //	["Jane", "Kim"]
+    for(let i=0; i<seoul.length; i++){
+        if(seoul[i]==="Kim"){
+            return `김서방은 ${i}에 있다`; //	"김서방은 1에 있다"
+        }
+    }
+}
