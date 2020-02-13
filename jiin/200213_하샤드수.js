304d3d6e62758dc9101c4f46b8bc84eb686ace97
jiindev <jiindev@gmail.com>
2020-02-13 23:36:41 +0900

    jiin / 하샤드 수

---
+++ jiin/200213_하샤드수.js
@@ -0,0 +1,9 @@
+function solution(x) { //10,12,11,13
+    let result;
+    let num = String(x);
+    let plus = 0;
+    for(let i=0; i<num.length; i++){
+        plus += Number(num[i]);
+    }
+    return x%plus===0?true:false; //true,true,false,false
+}
